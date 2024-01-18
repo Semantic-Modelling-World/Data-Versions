f275d451c270b44be0f96e04ebd66837407d1e34
Leonardo Mieschendahl <leonardo.mieschendahl@sick.de>
2024-01-18 09:42:32 +0000

    updated index.js and package.json

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path')
 
 const port = 3001;
 
-app.use('/', express.static(path.resolve(__dirname, '')));
+app.use('/', express.static(path.resolve(__dirname, 'public/')));
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
