50bb2bc3b67c94cc5e0abd972c55af6eafeb138a
Leonardo Mieschendahl <leonardo.mieschendahl@sick.de>
2024-01-31 12:47:03 +0000

    removed confusing comment

--- public/sources/main.js
+++ public/sources/main.js
@@ -1,8 +1,6 @@
 document.addEventListener('contextmenu', event => event.preventDefault());
 
 const MAIN = (p5) => {
-  // p5 is only available in a function context, which is why we wrap all code
-  // into functions and exchange variables via the exp argument.
   const canvas = p5.createCanvas(p5.windowWidth, p5.windowHeight);
   var exp = { p5: p5, id: 0, canvas };
   UTILS(exp);
